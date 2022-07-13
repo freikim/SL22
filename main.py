2b90cccbf464cbfa8d51627ce203dd50c90c7a76
Kim Frei <kim@frei.dk>
2022-07-13 16:03:46 +0200

    Elon

--- main.py
+++ main.py
@@ -111,7 +111,8 @@ class VideoScreen(Screen):
     idx = NumericProperty(0)
     videos = [
         'videos/vocodes_video_JWINF2kb4vy9hqph994zbmq0e0khchm.mp4',
-        'videos/vocodes_video_JWINFe8289c79rt5czjgp2w7t915tp1.mp4'
+        'videos/vocodes_video_JWINFe8289c79rt5czjgp2w7t915tp1.mp4',
+        'videos/vocodes_video_JWINFs0e28zvdpswjfdved778xxhasm.mp4'
     ]
 
     def on_keyboard(self, key):
